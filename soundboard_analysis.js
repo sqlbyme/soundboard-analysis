7100c50a99b6c6604e45cebc0fcc89de5c27217b
Mike Edwards <mike@songbirdnest.com>
2012-11-05 14:38:45 -0800

    Trying to add html code to report.

--- soundboard_analysis.js
+++ soundboard_analysis.js
@@ -39,7 +39,10 @@ function IsAM(d) {
 // end IsAm
 
 // try html
-print("<html><head /><body>");
+print("<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>");
+print("<html xmlns='http://www.w3.org/1999/xhtml'>");
+print("<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'>");
+print("</head>");
 
 // Output the report header
 print("******************************");
